9fef0fe087e285a976c88e513c60c1f49fbd5acb
pretomhira <freelancer.pretomhir@gmail.com>
2023-05-24 19:37:37 +0600

    setup basic stucture

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,9 @@
+export default function Experience() {
+  return (
+    <main>
+      <div className="uppercase font-bold">
+        <h1>Experience</h1>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="uppercase font-bold">
+        <h1>Home</h1>
+      </div>
+    </main>
+  );
+}
