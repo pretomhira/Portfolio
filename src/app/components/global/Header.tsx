f4d7e595936e5c968ae41ed2d7646aebe2d31a68
pretomhira <freelancer.pretomhir@gmail.com>
2023-06-11 01:23:55 +0600

    header active link style updated

--- src/app/components/global/Header.tsx
+++ src/app/components/global/Header.tsx
@@ -1,19 +1,41 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function () {
+  const pathname = usePathname();
   return (
     <div className="bg-black/10 fixed w-full">
       <div className="text-white flex flex-row justify-center gap-10 p-2  max-w-7xl mx-auto">
-        <div className="text-3xl font-medium">Welcome</div>
+        <Link href="/" className="text-3xl font-medium">
+          Welcome
+        </Link>
         <div className="flex flex-col justify-center">
           <div className="flex flex-row justify-end gap-5">
-            <div className=" capitalize text-xl hover:text-teal-100  hover:shadow-sm hover:shadow-teal-200 p-2 rounded-md">
+            <Link
+              href="/experience"
+              className={` capitalize text-xl shadow-md hover:shadow-teal-300 hover:text-teal-200 p-2 rounded-md duration-700 ${
+                pathname.endsWith("/experience") && "shadow-cyan-700"
+              }`}
+            >
               experience
-            </div>
-            <div className=" capitalize text-xl hover:text-teal-100  hover:shadow-sm hover:shadow-teal-200 p-2 rounded-md">
+            </Link>
+            <Link
+              href="/education"
+              className={` capitalize text-xl shadow-md hover:shadow-teal-300 hover:text-teal-200 p-2 rounded-md duration-700 ${
+                pathname.endsWith("/education") && "shadow-cyan-700"
+              }`}
+            >
               education
-            </div>
-            <div className=" capitalize text-xl hover:text-teal-100  hover:shadow-sm hover:shadow-teal-200 p-2 rounded-md">
-              projects
-            </div>
+            </Link>
+            <Link
+              href="/project"
+              className={` capitalize text-xl shadow-md hover:shadow-teal-300 hover:text-teal-200 p-2 rounded-md duration-700 ${
+                pathname.endsWith("/project") && "shadow-cyan-700"
+              }`}
+            >
+              project
+            </Link>
           </div>
         </div>
       </div>
