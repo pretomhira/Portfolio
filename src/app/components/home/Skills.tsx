c33871d3b3c6e0c634615a9eafe3a32662b7ccf7
pretomhira <freelancer.pertomhir@gmail.com>
2023-07-09 00:15:41 +0600

    added interection observer, only once triggered

--- src/app/components/home/Skills.tsx
+++ src/app/components/home/Skills.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 // DataTypes
 import { SkillPropsInfo, SkillDetail, Level } from "@/app/api/dataTypes";
@@ -8,38 +8,41 @@ import { SkillPropsInfo, SkillDetail, Level } from "@/app/api/dataTypes";
 // Functions
 const levelValue: Function = (level: Level) => {
   if (level === "expart") {
-    return "w-full";
+    return "max-w-full";
   }
   if (level === "proficient") {
-    return "w-[85%]";
+    return "max-w-[85%]";
   }
   if (level === "competent") {
-    return "w-[75%]";
+    return "max-w-[75%]";
   }
 };
 
 export default function Skills({ skillInfo }: SkillPropsInfo): JSX.Element {
   const myRef = useRef(null);
-
+  const [isVisible, setIsVisible] = useState(false);
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       const entry = entries[0];
-      console.log("entry", entry);
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+      }
+      setIsVisible(entry.isIntersecting);
     });
+
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     observer.observe(myRef.current!);
-  });
-  console.log(myRef);
+  }, []);
+
   return (
     <div className="card-1 group">
       <div className="flex flex-col gap-5">
         <div className="title-2">Skills</div>
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5" ref={myRef}>
           {skillInfo.map((skill: SkillDetail) => (
             <div
               key={skill.id}
               className="flex flex-col sm:flex-col gap-3 border border-teal-800 m-1 p-3 rounded-md"
-              ref={myRef}
             >
               <div className="title-4 !w-72">{skill.key}</div>
               <div className="cursor-copy flex flex-col gap-2">
@@ -55,9 +58,9 @@ export default function Skills({ skillInfo }: SkillPropsInfo): JSX.Element {
                       <div className="relative w-full">
                         <div className="h-3 bg-teal-800 max-w-md w-screen rounded-md"></div>
                         <div
-                          className={`justify-end absolute top-0 left-0 h-3 bg-teal-300 group-hover:bg-teal-100 duration-700 ${levelValue(
-                            level.key
-                          )} rounded-md`}
+                          className={`justify-end absolute top-0 left-0 h-3 bg-teal-300 group-hover:bg-teal-100 duration-700  ${
+                            isVisible ? "animate-myGrow" : "animate-none w-5"
+                          } ${levelValue(level.key)} rounded-md`}
                         >
                           <div className="relative w-full h-full">
                             <div className="absolute -top-1 right-0 h-5 w-5  bg-cyan-100 rounded-full"></div>
